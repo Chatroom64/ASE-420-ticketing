494926c9c4ebf64a5c9dda8bb89faeb7efa85146
Pepper <56687049+echocall@users.noreply.github.com>
2024-11-29 16:23:01 -0500

    Database set up

---
+++ database/connect.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        #"""Initialize the database connection."""
+        self.db_name = db_name
+        self.connection = None
+
+    def connect(self):
+        # """Establish a connection to the database."""
+        if self.connection is None:
+            self.connection = sqlite3.connect(self.db_name)
+        return self.connection
+
+    def close(self):
+        # """Close the database connection."""
+        if self.connection:
+            self.connection.close()
+            self.connection = None
---
+++ database/setup.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+sql_statements = [ 
+    """CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY, 
+            name text NOT NULL, 
+            role INT NOT NULL, 
+            email text NOT NULL,
+            password text NOT NULL
+        );""",
+
+    """CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY, 
+            title TEXT NOT NULL, 
+            priority INT, 
+            status text,
+            creator_id INT NOT NULL, 
+            open_date DATE NOT NULL, 
+            close_date DATE, 
+            body text NOT NULL,
+            FOREIGN KEY (creator_id) REFERENCES users (id)
+        );""",
+
+    """CREATE TABLE IF NOT EXISTS reply (
+            id INTEGER PRIMARY KEY, 
+            title TEXT NOT NULL, 
+            priority INT, 
+            status text,
+            creator_id INT NOT NULL, 
+            post_date DATE NOT NULL, 
+            body text NOT NULL,
+            ticket_id INT NOT NULL,
+            FOREIGN KEY (creator_id) REFERENCES users (id),
+            FOREIGN KEY (ticket_id) REFERENCES tickets (id)
+        );""",
+
+]
+
+# create a database connection
+try:
+    with sqlite3.connect('data.db') as conn:
+        # create a cursor
+        cursor = conn.cursor()
+
+        # execute statements
+        for statement in sql_statements:
+            cursor.execute(statement)
+
+        # commit the changes
+        conn.commit()
+
+        print("Tables created successfully.")
+except sqlite3.OperationalError as e:
+    print("Failed to create tables:", e)
